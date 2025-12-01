0a9d913993747f22ddf491720f96754a93d4ac00
Teixeira212 <andre.dt@puccampinas.edu.br>
2025-11-30 21:55:40 -0300

    Implementando Landing Page

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Rotas da API
+app.use("/api/landingPage", authRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/plantio", plantioRoutes);
 
 // Rotas de página
+app.get("/landingPage", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "landingPage.html"));
+});
+
 app.get("/auth", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "auth.html"));
 });
